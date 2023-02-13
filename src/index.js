69a547b36e22b3fd6e311083cecfa5aa8e194726
Classy <69457423+Classy08@users.noreply.github.com>
2023-02-13 20:52:53 +0530

    mouse issue and performance after few games fix

--- src/index.js
+++ src/index.js
@@ -1,9 +1,11 @@
 require('v8-compile-cache');
 
-const { app, BrowserWindow, globalShortcut, protocol, ipcMain, dialog, clipboard } = require('electron');
+const { app, BrowserWindow, protocol, ipcMain, dialog, clipboard } = require('electron');
 app.startedAt = Date.now();
 const path = require('path');
 const official_settings = ['Unlimited FPS', 'Accelerated Canvas', 'Game Capture'];
+const fs = require('fs')
+var downloadPath = path.normalize(`${app.getPath('appData')}\\vengeclient\\userscript\\`);
 
 const shortcuts = require('electron-localshortcut');
 
@@ -19,8 +21,16 @@ Store.initRenderer();
 const settings = new Store({
     defaults: {
         'Unlimited FPS': true,
+        'Game Capture': false,
+        'userscript': downloadPath,
         'Accelerated Canvas': false,
-        'Game Capture': false
+        'Game Capture': false,
+        'remove-useless': true,
+        'helpful-flag': true,
+        'flag-limit-increase': true,
+        'low latency': false,
+        'Experimental Flags': false,
+        'gpu-rasterization': true
     }
 });
 
@@ -34,54 +44,80 @@ const rpc_script = require('./rpc.js');
 //swapper_func
 const swapper = require('./swapper.js');
 const { machine } = require('os');
+const { download } = require('electron-dl');
+
+//Flags
+
+if (settings.get('remove-useless')) {
+    app.commandLine.appendSwitch('disable-breakpad');
+    app.commandLine.appendSwitch('disable-print-preview');
+    app.commandLine.appendSwitch('disable-metrics-repo');
+    app.commandLine.appendSwitch('disable-metrics');
+    app.commandLine.appendSwitch('disable-2d-canvas-clip-aa');
+    app.commandLine.appendSwitch('disable-bundled-ppapi-flash');
+    app.commandLine.appendSwitch('disable-logging');
+    app.commandLine.appendSwitch('disable-hang-monitor');
+    app.commandLine.appendSwitch('disable-component-update');
+    if (process.platform === 'darwin') app.commandLine.appendSwitch('disable-dev-shm-usage');
+}
+
+if (settings.get('helpful-flag')) {
+    app.commandLine.appendSwitch('in-process-gpu');
+    app.commandLine.appendSwitch('enable-javascript-harmony');
+    app.commandLine.appendSwitch('enable-future-v8-vm-features');
+    app.commandLine.appendSwitch('enable-webgl');
+    app.commandLine.appendSwitch('enable-webgl2-compute-context');
+    app.commandLine.appendSwitch('disable-background-timer-throttling');
+    app.commandLine.appendSwitch('disable-renderer-backgrounding');
+    app.commandLine.appendSwitch('autoplay-policy', 'no-user-gesture-required');
+}
+
+if (settings.get('flag-limit-increase')) {
+    app.commandLine.appendSwitch('renderer-process-limit', '100');
+    app.commandLine.appendSwitch('max-active-webgl-contexts', '100');
+    app.commandLine.appendSwitch('webrtc-max-cpu-consumption-percentage', '100');
+    app.commandLine.appendSwitch('ignore-gpu-blacklist');
+}
+if (settings.get('low latency')) {
+    app.commandLine.appendSwitch('enable-highres-timer');
+    app.commandLine.appendSwitch('enable-quic');
+    app.commandLine.appendSwitch('enable-accelerated-2d-canvas');
+}
+if (settings.get('Experimental Flags')) {
+    app.commandLine.appendSwitch('disable-low-end-device-mode');
+    app.commandLine.appendSwitch('enable-accelerated-video-decode');
+    app.commandLine.appendSwitch('enable-native-gpu-memory-buffers');
+    app.commandLine.appendSwitch('high-dpi-support', '1');
+    app.commandLine.appendSwitch('ignore-gpu-blacklist');
+    app.commandLine.appendSwitch('no-pings');
+    app.commandLine.appendSwitch('no-proxy-server');
+}
+if (settings.get('gpu-rasterization')) {
+    app.commandLine.appendSwitch('enable-gpu-rasterization');
+    app.commandLine.appendSwitch('enable-oop-rasterization');
+    app.commandLine.appendSwitch('disable-zero-copy');
+}
 
 
 //Uncap FPS
-app.commandLine.appendSwitch("force_high_performance_gpu");
-app.commandLine.appendSwitch("in-process-gpu");
-app.commandLine.appendSwitch("disable-backgrounding-occluded-windows");
-app.commandLine.appendSwitch("smooth-scrolling");
-app.commandLine.appendSwitch("force-high-performance-gpu");
-app.commandLine.appendSwitch("disable-breakpad");
-app.commandLine.appendSwitch("disable-component-update");
-app.commandLine.appendSwitch("disable-print-preview");
-app.commandLine.appendSwitch("disable-metrics");
-app.commandLine.appendSwitch("disable-metrics-repo");
-app.commandLine.appendSwitch("enable-javascript-harmony");
-app.commandLine.appendSwitch("enable-future-v8-vm-features");
-app.commandLine.appendSwitch("enable-webgl2-compute-context");
-app.commandLine.appendSwitch("disable-hang-monitor");
-app.commandLine.appendSwitch("no-referrers");
-app.commandLine.appendSwitch("renderer-process-limit", 100);
-app.commandLine.appendSwitch("max-active-webgl-contexts", 100);
-app.commandLine.appendSwitch("enable-quic");
-app.commandLine.appendSwitch("high-dpi-support", 1);
-app.commandLine.appendSwitch("ignore-gpu-blacklist");
-app.commandLine.appendSwitch("disable-2d-canvas-clip-aa");
-app.commandLine.appendSwitch("disable-bundled-ppapi-flash");
-app.commandLine.appendSwitch("disable-logging");
-app.commandLine.appendSwitch("disable-web-security");
-app.commandLine.appendSwitch("webrtc-max-cpu-consumption-percentage=100");
-app.commandLine.appendSwitch('webrtc-max-cpu-consumption-percentage', '100')
-
 
 if (settings.get('Unlimited FPS')) {
     app.commandLine.appendSwitch('disable-frame-rate-limit');
     app.commandLine.appendSwitch('disable-gpu-vsync');
+    app.commandLine.appendSwitch('max-gum-fps', '9999');
 }
 
+//Capture Mode
 if (settings.get("Game Capture")) {
     const os = require('os');
     if (os.cpus()[0].model.indexOf('AMD') > -1)
         app.commandLine.appendSwitch('enable-zero-copy');
-    app.commandLine.appendSwitch('disable-direct-composition');
+    app.commandLine.appendSwitch('disable-gpu-compositing')
+    app.commandLine.appendSwitch('disable-accelerated-video-decode')
+    app.commandLine.appendSwitch('disable-accelerated-video-encode')
 }
 
-//acceleratedCanvas
-if (settings.get('Accelerated Canvas') === undefined) settings.set('Accelerated Canvas', false);
-if (settings.get('Accelerated Canvas')) {
-    app.commandLine.appendSwitch('enable-accelerated-2d-canvas');
-}
+app.allowRendererProcessReuse = true;
 
 //main Client Code
 const createWindow = () => {
@@ -126,7 +162,11 @@ const createWindow = () => {
     shortcuts.register(win, "F6", () => { if (clipboard.readText().includes("venge.io")) { win.loadURL(clipboard.readText()) } })
     shortcuts.register(win, 'F11', () => { win.fullScreen = !win.fullScreen; settings.set('Fullscreen', win.fullScreen) });
     shortcuts.register(win, "F12", () => win.webContents.toggleDevTools());
+    shortcuts.register('F2', async () => {
+        loadHub();
+    });
     shortcuts.register(win, "Escape", () => win.webContents.executeJavaScript('document.exitPointerLock()', true));
+
 
 
     //Auto Update
@@ -171,12 +211,24 @@ const createWindow = () => {
     win.webContents.on('dom-ready', () => {
         ipcMain.on('loadScripts', function (event) {
             swapper.runScripts(win, app);
+            swapper.initScripts(win, app)
             event.sender.send('scriptsLoaded', true);
         });
 
         swapper.replaceResources(win, app);
     })
 
+    ipcMain.on('click', (event, data) => {
+        download(win, data.url, { "directory": downloadPath })
+    })
+
+    ipcMain.on('exit', () => {
+        app.exit();
+    });
+
+    ipcMain.on('LoadHub', () => {
+        loadHub()
+    })
 
     //Discord RPC
 
@@ -222,12 +274,31 @@ const createWindow = () => {
             if (setting.name == 'Unlimited FPS') { app.exit(); app.relaunch(); }
             if (setting.name == 'Accelerated Canvas') { app.exit(); app.relaunch(); }
             if (setting.name == 'Game Capture') { app.exit(); app.relaunch(); }
-            
+
             console.log(setting.name)
         }
     });
+
+
 }
 
+function loadHub() {
+    let HubWindow = new BrowserWindow({
+        width: 1200,
+        height: 800,
+        title: `Venge Client`,
+        backgroundColor: '#202020',
+        icon: __dirname + "/icon.ico",
+        webPreferences: {
+            preload: __dirname + '/userscript/script.js',
+            nodeIntegration: false,
+        }
+    });
+
+    //HubWindow.removeMenu()
+    HubWindow.loadFile(path.join(__dirname, 'userscript/index.html'));
+    HubWindow.savedTitle = 'URL Menu';
+}
 
 app.whenReady().then(() => {
     protocol.registerFileProtocol('swap', (request, callback) => {
@@ -245,10 +316,8 @@ app.whenReady().then(() => {
 
 })
 
-app.on("window-all-closed", () => {
-    if (process.platform !== "darwin") {
-        app.quit();
-    }
-});
+app.on('window-all-closed', () => {
+    app.quit()
+})
 
 RPC.login({ clientId }).catch(console.error);
