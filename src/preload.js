050e5590ba2d79ac90860791c16a0122a89c9202
Classy <69457423+Classy08@users.noreply.github.com>
2023-02-19 10:45:32 +0530

    Update preload.js

--- src/preload.js
+++ src/preload.js
@@ -7,53 +7,53 @@ let loadChecker = setInterval(() => {
     }
 }, 500);
 
-window.onload = () => {
-    changePage(app.page, app.tab);
-}
+// window.onload = () => {
+//     changePage(app.page, app.tab);
+// }
 
-function changePage(page, tab) {
-    if (page == 'Home') {
-        if (!document.getElementById('ClientExit')) {
-            var div = document.getElementsByClassName('menu-button shop')[0];
-            if (div) div.parentElement.insertAdjacentHTML('beforeend', '<a aria-label="Exit" id="ClientExit" class="menu-button settings hint--left button-sound" style="cursor: pointer;"><img src="images/Close-Icon.png"></a>')
+// function changePage(page, tab) {
+//     if (page == 'Home') {
+//         if (!document.getElementById('ClientExit')) {
+//             var div = document.getElementsByClassName('menu-button shop')[0];
+//             if (div) div.parentElement.insertAdjacentHTML('beforeend', '<a aria-label="Exit" id="ClientExit" class="menu-button settings hint--left button-sound" style="cursor: pointer;"><img src="images/Close-Icon.png"></a>')
 
-            var div = document.getElementsByClassName('menu-button shop')[0];
+//             var div = document.getElementsByClassName('menu-button shop')[0];
 
-            if (div) div.parentElement.insertAdjacentHTML('beforeend', '<a aria-label="Client Hub" id="ClientHub" class="menu-button settings hint--left button-sound" style="cursor: pointer;"><img src="https://cdn.discordapp.com/attachments/925622910570336296/1066011519478931567/4703487_1.png"></a>')
+//             if (div) div.parentElement.insertAdjacentHTML('beforeend', '<a aria-label="Client Hub" id="ClientHub" class="menu-button settings hint--left button-sound" style="cursor: pointer;"><img src="https://cdn.discordapp.com/attachments/925622910570336296/1066011519478931567/4703487_1.png"></a>')
 
-            document.getElementById('ClientExit').onclick = function () { ipcRenderer.send("exit") }
-            document.getElementById('ClientHub').onclick = function () { ipcRenderer.send("LoadHub") }
-        }
-    }
-}
+//             document.getElementById('ClientExit').onclick = function () { ipcRenderer.send("exit") }
+//             document.getElementById('ClientHub').onclick = function () { ipcRenderer.send("LoadHub") }
+//         }
+//     }
+// }
 
-ipcRenderer.on('scriptsLoaded', (event, hasScripts) => {
-    if (hasScripts) {
-        app.settingsTabs.push({ name: 'Client', icon: 'fa-desktop', tab: 'Client' });
-        app.customSettings.push({ name: 'Client Message', type: 'message', value: 'Official Client Settings', tab: 'Client' });
-        app.customSettings.push({ name: 'Unlimited FPS', type: 'checkbox', value: true, tab: 'Client' });
-        app.customSettings.push({ name: 'info-flags', type: 'message', value: 'Performance Settins of the client', tab: 'Client' });
-        app.customSettings.push({ name: 'Gpu Rasterization', type: 'checkbox', value: true, tab: 'Client' });
-        app.customSettings.push({ name: 'flag-limit-increase', type: 'checkbox', value: true, tab: 'Client' });
-        app.customSettings.push({ name: 'low latency', type: 'checkbox', value: false, tab: 'Client' });
-        app.customSettings.push({ name: 'Experimental Flags', type: 'checkbox', value: false, tab: 'Client' });
-        app.customSettings.push({ name: 'GameCapture', type: 'message', value: 'Gives better peroformance if you are recording/streaming your game.', tab: 'Client' });
-        app.customSettings.push({ name: 'Game Capture', type: 'checkbox', value: false, tab: 'Client' });
+// ipcRenderer.on('scriptsLoaded', (event, hasScripts) => {
+//     if (hasScripts) {
+//         app.settingsTabs.push({ name: 'Client', icon: 'fa-desktop', tab: 'Client' });
+//         app.customSettings.push({ name: 'Client Message', type: 'message', value: 'Official Client Settings', tab: 'Client' });
+//         app.customSettings.push({ name: 'Unlimited FPS', type: 'checkbox', value: true, tab: 'Client' });
+//         app.customSettings.push({ name: 'info-flags', type: 'message', value: 'Performance Settins of the client', tab: 'Client' });
+//         app.customSettings.push({ name: 'Gpu Rasterization', type: 'checkbox', value: true, tab: 'Client' });
+//         app.customSettings.push({ name: 'flag-limit-increase', type: 'checkbox', value: true, tab: 'Client' });
+//         app.customSettings.push({ name: 'low latency', type: 'checkbox', value: false, tab: 'Client' });
+//         app.customSettings.push({ name: 'Experimental Flags', type: 'checkbox', value: false, tab: 'Client' });
+//         app.customSettings.push({ name: 'GameCapture', type: 'message', value: 'Gives better peroformance if you are recording/streaming your game.', tab: 'Client' });
+//         app.customSettings.push({ name: 'Game Capture', type: 'checkbox', value: false, tab: 'Client' });
 
-        pc.app.on('Client:CustomSettingsChange', function (setting) {
-            ipcRenderer.send('settingChange', setting);
-        });
+//         pc.app.on('Client:CustomSettingsChange', function (setting) {
+//             ipcRenderer.send('settingChange', setting);
+//         });
 
-        pc.app.on('Player:Leave', function () {
-            ipcRenderer.send('loadRPC', { area: 'menu' });
-        }, this);
+//         pc.app.on('Player:Leave', function () {
+//             ipcRenderer.send('loadRPC', { area: 'menu' });
+//         }, this);
 
-        pc.app.on('Overlay:Weapon', function (weapon) {
-            let maps = app.session.defaultMaps.map((map) => map.split(' - ')[0]);
-            ipcRenderer.send('loadRPC', { area: 'game', now: Date.now(), weapon: weapon, map: app.session.map, maps: maps, mapText: app.session.defaultMaps.filter((map) => map.split(' - ')[0] == app.session.map)[0] });
-        }, this);
+//         pc.app.on('Overlay:Weapon', function (weapon) {
+//             let maps = app.session.defaultMaps.map((map) => map.split(' - ')[0]);
+//             ipcRenderer.send('loadRPC', { area: 'game', now: Date.now(), weapon: weapon, map: app.session.map, maps: maps, mapText: app.session.defaultMaps.filter((map) => map.split(' - ')[0] == app.session.map)[0] });
+//         }, this);
 
 
-        console.log('----------\nScripts Loaded\n----------');
-    }
-});
+//         console.log('----------\nScripts Loaded\n----------');
+//     }
+// });
