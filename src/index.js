ac9f31ea10cb84aabeb959673378bc3476f5862f
Classy <69457423+Classy08@users.noreply.github.com>
2023-03-01 07:29:45 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -20,7 +20,7 @@ const Store = require('electron-store');
 Store.initRenderer();
 const settings = new Store({
     defaults: {
-        'Unlimited FPS': true,
+        'UncapFPS': true,
         'Game Capture': false,
         'userscript': downloadPath,
         'Accelerated Canvas': false,
@@ -29,7 +29,8 @@ const settings = new Store({
         'flag-limit-increase': true,
         'low latency': false,
         'Experimental Flags': false,
-        'gpu-rasterization': true
+        'gpu-rasterization': true,
+        'Fullscreen': false
     }
 });
 
@@ -38,90 +39,23 @@ const DiscordRPC = require('discord-rpc');
 const clientId = '727533470594760785';
 const RPC = new DiscordRPC.Client({ transport: 'ipc' });
 DiscordRPC.register(clientId);
-const rpc_script = require('./rpc.js');
+const rpc_script = require('./modules/rpc.js');
 
 //swapper_func
-const swapper = require('./swapper.js');
+const swapper = require('./modules/swapper.js');
 const { machine } = require('os');
 const { download } = require('electron-dl');
+
 //exit
 ipcMain.on('exit', () => {
     app.exit();
 });
+//launch Args (thanks to CreepyCats)
+const launchArgs = require('./modules/launchArgs.js');
+launchArgs.pushArguments()
 
-//Flags
-
-if (settings.get('remove-useless')) {
-    app.commandLine.appendSwitch('disable-breakpad');
-    app.commandLine.appendSwitch('disable-print-preview');
-    app.commandLine.appendSwitch('disable-metrics-repo');
-    app.commandLine.appendSwitch('disable-metrics');
-    app.commandLine.appendSwitch('disable-2d-canvas-clip-aa');
-    app.commandLine.appendSwitch('disable-bundled-ppapi-flash');
-    app.commandLine.appendSwitch('disable-logging');
-    app.commandLine.appendSwitch('disable-hang-monitor');
-    app.commandLine.appendSwitch('disable-component-update');
-    if (process.platform === 'darwin') app.commandLine.appendSwitch('disable-dev-shm-usage');
-}
-
-if (settings.get('helpful-flag')) {
-    app.commandLine.appendSwitch('in-process-gpu');
-    app.commandLine.appendSwitch('enable-javascript-harmony');
-    app.commandLine.appendSwitch('enable-future-v8-vm-features');
-    app.commandLine.appendSwitch('enable-webgl');
-    app.commandLine.appendSwitch('enable-webgl2-compute-context');
-    app.commandLine.appendSwitch('disable-background-timer-throttling');
-    app.commandLine.appendSwitch('disable-renderer-backgrounding');
-    app.commandLine.appendSwitch('autoplay-policy', 'no-user-gesture-required');
-}
-
-if (settings.get('flag-limit-increase')) {
-    app.commandLine.appendSwitch('renderer-process-limit', '100');
-    app.commandLine.appendSwitch('max-active-webgl-contexts', '100');
-    app.commandLine.appendSwitch('webrtc-max-cpu-consumption-percentage', '100');
-    app.commandLine.appendSwitch('ignore-gpu-blacklist');
-}
-if (settings.get('low latency')) {
-    app.commandLine.appendSwitch('enable-highres-timer');
-    app.commandLine.appendSwitch('enable-quic');
-    app.commandLine.appendSwitch('enable-accelerated-2d-canvas');
-}
-if (settings.get('Experimental Flags')) {
-    app.commandLine.appendSwitch('disable-low-end-device-mode');
-    app.commandLine.appendSwitch('enable-accelerated-video-decode');
-    app.commandLine.appendSwitch('enable-native-gpu-memory-buffers');
-    app.commandLine.appendSwitch('high-dpi-support', '1');
-    app.commandLine.appendSwitch('ignore-gpu-blacklist');
-    app.commandLine.appendSwitch('no-pings');
-    app.commandLine.appendSwitch('no-proxy-server');
-}
-if (settings.get('gpu-rasterization')) {
-    app.commandLine.appendSwitch('enable-gpu-rasterization');
-    app.commandLine.appendSwitch('enable-oop-rasterization');
-    app.commandLine.appendSwitch('disable-zero-copy');
-}
-
-
-//Uncap FPS
-
-if (settings.get('Unlimited FPS')) {
-    app.commandLine.appendSwitch('disable-frame-rate-limit');
-    app.commandLine.appendSwitch('disable-gpu-vsync');
-    app.commandLine.appendSwitch('max-gum-fps', '9999');
-}
-
-//Capture Mode
-if (settings.get("Game Capture")) {
-    const os = require('os');
-    if (os.cpus()[0].model.indexOf('AMD') > -1)
-        app.commandLine.appendSwitch('enable-zero-copy');
-    app.commandLine.appendSwitch('disable-gpu-compositing')
-    app.commandLine.appendSwitch('disable-accelerated-video-decode')
-    app.commandLine.appendSwitch('disable-accelerated-video-encode')
-}
 
 app.allowRendererProcessReuse = true;
-
 //main Client Code
 const createWindow = () => {
     const win = new BrowserWindow({
@@ -212,14 +146,12 @@ const createWindow = () => {
 
     //Swapper
     win.webContents.on('dom-ready', () => {
-//         ipcMain.on('loadScripts', function (event) {
-//             swapper.runScripts(win, app);
-//             event.sender.send('scriptsLoaded', true);
-//         });
 
         swapper.replaceResources(win, app);
     });
+
     swapper.initStyles(win, app)
+
     // ipcMain.on('click', (event, data) => {
     //     download(win, data.url, { "directory": downloadPath })
     // })
@@ -234,52 +166,52 @@ const createWindow = () => {
 
     //Discord RPC
 
-    // ipcMain.on('loadRPC', (event, data) => {
-    //     if (data.area == 'game') {
-    //         rpc_script.setActivity(RPC, {
-    //             state: 'In a game',
-    //             startTimestamp: data.now,
-    //             largeImageKey: data.maps.includes(data.map) ? data.map.toLowerCase() : 'custom',
-    //             largeImageText: data.mapText == undefined ? data.map + ' - CUSTOM MATCH' : data.mapText,
-    //             smallImageKey: data.weapon.toLowerCase(),
-    //             smallImageText: data.weapon,
-    //             instance: false,
-    //             buttons: [
-    //                 {
-    //                     label: 'Download Client',
-    //                     url: 'https://social.venge.io/client.html'
-    //                 }
-    //             ]
-    //         });
-    //     }
+    ipcMain.on('loadRPC', (event, data) => {
+        if (data.area == 'game') {
+            rpc_script.setActivity(RPC, {
+                state: 'In a game',
+                startTimestamp: data.now,
+                largeImageKey: data.maps.includes(data.map) ? data.map.toLowerCase() : 'custom',
+                largeImageText: data.mapText == undefined ? data.map + ' - CUSTOM MATCH' : data.mapText,
+                smallImageKey: data.weapon.toLowerCase(),
+                smallImageText: data.weapon,
+                instance: false,
+                buttons: [
+                    {
+                        label: 'Download Client',
+                        url: 'https://social.venge.io/client.html'
+                    }
+                ]
+            });
+        }
 
-    //     if (data.area == 'menu') {
-    //         rpc_script.setActivity(RPC, {
-    //             state: 'On the menu',
-    //             startTimestamp: app.startedAt,
-    //             largeImageKey: 'menu',
-    //             largeImageText: 'Venge.io',
-    //             instance: false,
-    //             buttons: [
-    //                 {
-    //                     label: 'Download Client',
-    //                     url: 'https://social.venge.io/client.html'
-    //                 }
-    //             ]
-    //         });
-    //     }
-    // });
+        if (data.area == 'menu') {
+            rpc_script.setActivity(RPC, {
+                state: 'On the menu',
+                startTimestamp: app.startedAt,
+                largeImageKey: 'menu',
+                largeImageText: 'Venge.io',
+                instance: false,
+                buttons: [
+                    {
+                        label: 'Download Client',
+                        url: 'https://social.venge.io/client.html'
+                    }
+                ]
+            });
+        }
+    });
 
-    // ipcMain.on('settingChange', function (event, setting) {
-    //     if (official_settings.includes(setting.name)) {
-    //         settings.set(setting.name, setting.value);
-    //         if (setting.name == 'Unlimited FPS') { app.exit(); app.relaunch(); }
-    //         if (setting.name == 'Accelerated Canvas') { app.exit(); app.relaunch(); }
-    //         if (setting.name == 'Game Capture') { app.exit(); app.relaunch(); }
+    ipcMain.on('settingChange', function (event, setting) {
+        if (official_settings.includes(setting.name)) {
+            settings.set(setting.name, setting.value);
+            if (setting.name == 'Unlimited FPS') { app.exit(); app.relaunch(); }
+            if (setting.name == 'Accelerated Canvas') { app.exit(); app.relaunch(); }
+            if (setting.name == 'Game Capture') { app.exit(); app.relaunch(); }
 
-    //         console.log(setting.name)
-    //     }
-    // });
+            console.log(setting.name)
+        }
+    });
 
 
 }
